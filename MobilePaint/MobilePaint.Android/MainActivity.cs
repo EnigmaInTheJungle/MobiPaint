09023f2a1dcd50b344ac030bd840b8b8aafa006d
Kulinich Vlad <kulinich_v@hotmail.com>
2017-10-06 06:38:16 +0300

    Created MobilePaint Prototype

--- MobilePaint/MobilePaint.Android/MainActivity.cs
+++ MobilePaint/MobilePaint.Android/MainActivity.cs
@@ -1,35 +1,108 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using SupportToolbar = Android.Support.V7.Widget.Toolbar;
+using SupportFragment = Android.Support.V4.App.Fragment;
+using Android.Support.V7.App;
+using Android.Support.V4.Widget;
+using System.Collections.Generic;
+using Android.Support.V7.Widget;
 
 namespace MobilePaint.Droid
 {
-	[Activity (Label = "MobilePaint.Android", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity
-	{
-		int count = 1;
+    [Activity(Label = "MobilePaint.Android", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/MyTheme")]
+    public class MainActivity : ActionBarActivity
+    {
+        private SupportToolbar mToolbar;
+        private SupportToolbar bToolbar;
+        private LeftMenuToggler mDrawerToggle;
+        private DrawerLayout mDrawerLayout;
+        private ListView mLeftDrawer;
 
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
-		}
-	}
+            mToolbar = FindViewById<SupportToolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(mToolbar);
+
+            bToolbar = FindViewById<SupportToolbar>(Resource.Id.lower_toolbar);
+            bToolbar.InflateMenu(Resource.Menu.bot_menu);
+            bToolbar.MenuItemClick += bToolbar_Click;
+
+            mDrawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            mLeftDrawer = FindViewById<ListView>(Resource.Id.left_menu);
+
+            Dictionary<string, string[]> menu = new Dictionary<string, string[]>();
+            menu.Add("File", new string[] { "New", "Open", "Save", "Save as..", "Save in cloud", "Load from cloud", "Exit" });
+            menu.Add("Window", new string[] { "Rename", "Close", "Close all"});
+            menu.Add("Plugins", new string[] { "Simple Figure", "Figure with text"});
+            menu.Add("Skins", new string[] { "Dark", "Light", "Pink"});
+            menu.Add("Language", new string[] { "English", "Ukrainian", "Russian"});
+            menu.Add("Help", new string[] { "About..", "Show help.."});
+
+
+            mLeftDrawer.Adapter = new CustomAdapter(this, menu);
+
+            mDrawerToggle = new LeftMenuToggler(
+                this,                           //Host Activity
+                mDrawerLayout,                  //DrawerLayout
+                Resource.String.openDrawer,     //Opened Message
+                Resource.String.closeDrawer     //Closed Message
+            );
+
+            mDrawerLayout.SetDrawerListener(mDrawerToggle);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.SetHomeButtonEnabled(true);
+            SupportActionBar.SetDisplayShowTitleEnabled(false);
+            mDrawerToggle.SyncState();
+
+        }
+
+        private void bToolbar_Click(object sender, SupportToolbar.MenuItemClickEventArgs e)
+        {
+            switch (e.Item.ItemId)
+            {
+                case Resource.Id.action_newtab: Toast.MakeText(this, "NewTab clicked", ToastLength.Short).Show(); break;
+                case Resource.Id.action_color: Toast.MakeText(this, "Color clicked", ToastLength.Short).Show(); break;
+                case Resource.Id.action_figure: Toast.MakeText(this, "Figure clicked", ToastLength.Short).Show(); break;
+                case Resource.Id.action_width: Toast.MakeText(this, "Width clicked", ToastLength.Short).Show(); break;
+                case Resource.Id.action_text: Toast.MakeText(this, "Text clicked", ToastLength.Short).Show(); break;
+                case Resource.Id.action_image: Toast.MakeText(this, "Image clicked", ToastLength.Short).Show(); break;
+            }
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    mDrawerToggle.OnOptionsItemSelected(item);
+                    return true;
+                default:
+                    return base.OnOptionsItemSelected(item);
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.top_menu, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override void OnConfigurationChanged(Android.Content.Res.Configuration newConfig)
+        {
+            base.OnConfigurationChanged(newConfig);
+            mDrawerToggle.OnConfigurationChanged(newConfig);
+        }
+    }
 }
 
 
---
+++ Tests/Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Tests
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void TestCreatePerson()
+        {
+            app.Tap(c => c.Id("top_menu").All().Index(0));
+            app.Tap(c => c.Text("File"));
+            app.Tap(c => c.Text("New"));
+        }
+    }
+}
