d7b18dbc2af9bda5e5ce64b59280682c42e53b30
Kulinich Vlad <kulinich_v@hotmail.com>
2017-10-06 07:10:26 +0300

    Added autotests

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -25,12 +25,76 @@ namespace Tests
             app = AppInitializer.StartApp(platform);
         }
 
-        [Test]
-        public void TestCreatePerson()
+        [TestCase("New")]
+        [TestCase("Open")]
+        [TestCase("Save")]
+        [TestCase("Save as..")]
+        [TestCase("Save in cloud")]
+        [TestCase("Load from cloud")]
+        [TestCase("Exit")]
+        public void TestFileMenu(string item)
         {
-            app.Tap(c => c.Id("top_menu").All().Index(0));
-            app.Tap(c => c.Text("File"));
-            app.Tap(c => c.Text("New"));
+            TestMenuItem("File", item);
+        }
+
+        [TestCase("Rename")]
+        [TestCase("Close")]
+        [TestCase("Close all")]
+        public void TestWindowMenu(string item)
+        {
+            TestMenuItem("Window", item);
+        }
+
+        [TestCase("Simple Figure")]
+        [TestCase("Figure with text")]
+        public void TestPluginsMenu(string item)
+        {
+            TestMenuItem("Plugins", item);
+        }
+
+        [TestCase("Dark")]
+        [TestCase("Light")]
+        [TestCase("Pink")]
+        public void TestSkinsMenu(string item)
+        {
+            TestMenuItem("Skins", item);
+        }
+
+        [TestCase("English")]
+        [TestCase("Russian")]
+        [TestCase("Ukrainian")]
+        public void TestLangMenu(string item)
+        {
+            TestMenuItem("Language", item);
+        }
+
+        [TestCase("About..")]
+        [TestCase("Show help..")]
+        public void TestHelpMenu(string item)
+        {
+            TestMenuItem("Help", item);
+        }
+
+        [TestCase("action_newtab", "NewTab clicked")]
+        [TestCase("action_color", "Color clicked")]
+        [TestCase("action_figure", "Figure clicked")]
+        [TestCase("action_width", "Width clicked")]
+        [TestCase("action_text", "Text clicked")]
+        [TestCase("action_image", "Image clicked")]
+        public void TestToolBar(string item, string res)
+        {
+            app.Tap(c => c.Id(item));
+            app.WaitForElement(c => c.Text(res));
+            app.WaitForNoElement(c => c.Text(res));
+        }
+
+        public void TestMenuItem(string menu, string item)
+        {
+            app.Tap(c => c.Id("toolbar").All().Index(1));
+            app.Tap(c => c.Text(menu));
+            app.Tap(c => c.Text(item));
+            app.WaitForElement(c => c.Text(item));
+            app.WaitForNoElement(c => c.Text(item));
         }
     }
 }
